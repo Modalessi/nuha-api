7c6bad70a1c4ec63744bfa8530de513cca3fa02d
Mohammed Alessi <ma.al_essi@icloud.com>
2024-11-24 19:29:04 +0300

    add: main

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcom to nuha, best competitve programming platform")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Modalessi/nuha-api
+
+go 1.22.5
